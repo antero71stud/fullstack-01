f92a71dc63cc6a5b5eac38e1f031595327570e39
Antero Oikkonen <antero.oikkonen@gmail.com>
2018-01-29 21:14:35 +0200

    tehtävä 1.3

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,47 +2,54 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Kurssi = (props) => {
-    return <h1>{props.kurssi}</h1>
+    return <h1>{props.name}</h1>
  }
 
  const Sisalto = (props) => {
      console.log('props',props)
      return <div>
-         <Osa osa={props.part1} tehtavia={props.t1}/>
-         <Osa osa={props.part2} tehtavia={props.t2}/>
-         <Osa osa={props.part3} tehtavia={props.t3}/>
+        <Osa nimi={props.part1.nimi} tehtavia={props.part1.tehtavia}/>
+         <Osa nimi={props.part2.nimi} tehtavia={props.part2.tehtavia}/>
+         <Osa nimi={props.part3.nimi} tehtavia={props.part3.tehtavia}/>
      </div>
  }
 
  const Osa = (props) => {
-    return <div><p>{props.osa}</p>
-    <p>{props.tehtavia}</p></div>
+    return <p>{props.nimi}<br></br>
+    {props.tehtavia}</p>
  }
 
  const Yhteensa = (props) => {
      return <p>yhteensä {props.t1 + props.t2 + props.t3} tehtävää</p>
  }
 
-const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+ const App = () => {
+    const kurssi = 'Half Stack -sovelluskehitys'
+    const osa1 = {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10
+    }
+    const osa2 = {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7
+    }
+    const osa3 = {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14
+    }
 
-  return (
-    <div>
+   return (
+     <div>
       <Kurssi name={kurssi}/>
-      <Sisalto part1={osa1} t1={tehtavia1} part2={osa2}
-      t2={tehtavia2} part3={osa3} t3={tehtavia3}/> 
-      <Yhteensa t1={tehtavia1} t2={tehtavia2} t3={tehtavia3}/>
+      <Sisalto part1={osa1} part2={osa2} part3={osa3} 
+      /> 
+      <Yhteensa t1={osa1.tehtavia} t2={osa2.tehtavia} t3={osa3.tehtavia}/>
     </div>
   )
 }
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+    <App />,
+    document.getElementById('root')
+  )
+
